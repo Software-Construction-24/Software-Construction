7c993f6bd0668bbb8f395ed7b3f9d0f2cb167ad0
SarahNsereko <sarahnsereko59@gmail.com>
2024-02-05 00:49:09 +0300

    srp refactor

---
+++ Courserawork/A96409/SOLID WORK/solid.py
@@ -0,0 +1,7 @@
+# (SRP )
+class ReportGenerator:
+    def generate_report(self, employee):
+        if employee.role == "Manager":
+            print(f"Manager Report: {employee.name}")
+        elif employee.role == "Developer":
+            print(f"Developer Report: {employee.name}")
